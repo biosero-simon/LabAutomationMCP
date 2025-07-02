1504e08e503d19b0014a4c863fb21edb440936e5
biosero-simon <simoncoelho@biosero.com>
2025-07-02 12:15:35 -0700

    add resource to expose path to documentation file

--- MockRobotMCP/Resources/DocumentationResource.cs
+++ MockRobotMCP/Resources/DocumentationResource.cs
@@ -1,39 +1,15 @@
 using ModelContextProtocol.Protocol;
 using ModelContextProtocol.Server;
 using System.ComponentModel;
+using System.IO;
 
 namespace LabAutomationMCP;
 
 [McpServerResourceType]
 public class SimpleResourceType
 {
-    [McpServerResource(UriTemplate = "test://direct/text/resource", Name = "Direct Text Resource", MimeType = "text/plain")]
-    [Description("A direct text resource")]
-    public static string DirectTextResource() => "This is a direct resource";
+    [McpServerResource(UriTemplate = "docs://direct/text/resource", Name = "Path to Documentation", MimeType = "text/plain")]
+    [Description("The full path to the documentation resources. This resource provides a direct link to the documentation file.")]
+    public static string DirectTextResource() => Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Documentation", "SampleDocumentation.pdf");
 
-    [McpServerResource(UriTemplate = "test://template/resource/{id}", Name = "Template Resource")]
-    [Description("A template resource with a numeric ID")]
-    public static ResourceContents TemplateResource(RequestContext<ReadResourceRequestParams> requestContext, int id)
-    {
-        int index = id - 1;
-        if ((uint)index >= ResourceGenerator.Resources.Count)
-        {
-            throw new NotSupportedException($"Unknown resource: {requestContext.Params?.Uri}");
-        }
-
-        var resource = ResourceGenerator.Resources[index];
-        return resource.MimeType == "text/plain" ?
-            new TextResourceContents
-            {
-                Text = resource.Description!,
-                MimeType = resource.MimeType,
-                Uri = resource.Uri,
-            } :
-            new BlobResourceContents
-            {
-                Blob = resource.Description!,
-                MimeType = resource.MimeType,
-                Uri = resource.Uri,
-            };
-    }
 }
